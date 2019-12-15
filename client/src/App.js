e560957d62c00ea16c680d2152915b487b0bd2d8
Josh Sample <joshua.sample1@globalpay.com>
2019-12-15 16:02:22 -0500

    Master - Client set up - built nav and landing

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.scss';
+
+const App = () => (
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </>
+  </Router>
+
+);
+
+export default App;
